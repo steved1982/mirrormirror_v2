b173adfac789537bf7d58f009a9a5162d52597f1
Stephen Dance <steve@genielocker.com>
2016-04-01 09:18:11 +0100

    add 1 hour

--- public/js/clock.js
+++ public/js/clock.js
@@ -4,7 +4,7 @@ date = null;
 var update = function () {
     date = moment(new Date())
     var html = '<p>' + date.format('dddd, MMMM Do') + '</p>';
-    html += '<p id="time">' + date.format('HH:mm') + '</p>';
+    html += '<p id="time">' + math.round(1+date.format('HH')) + ":" + date.format('mm') + '</p>';
     datetime.html(html);
 };
 
