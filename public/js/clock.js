45b2d14681727a5e975c3d1672012aee3cca6d2b
Stephen Dance <steve@genielocker.com>
2016-04-01 10:06:39 +0100

    added 0

--- public/js/clock.js
+++ public/js/clock.js
@@ -4,8 +4,11 @@ date = null;
 var update = function () {
     date = moment(new Date());
     var myDate = new Date();
-    myTime =  ( myDate.getHours() + 1 );
+    myTime =  ( myDate.getHours() );
     myMins =  ( myDate.getMinutes() );
+    if (myMins < 10){
+        myMins = "0" + myMins;
+    }
 
     var html = '<p>' + date.format('dddd, MMMM Do') + '</p>';
     html += '<p id="time">' + myTime + ':' + myMins + '</p>';
