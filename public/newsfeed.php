0398a17ff6ef6a7ee4ef7c3a05ab516dc15f79ab
Stephen Dance <stephen.dance@lightmaker.com>
2016-11-02 10:27:12 +0000

    Removed image

--- public/newsfeed.php
+++ public/newsfeed.php
@@ -27,6 +27,5 @@ function sortFunction( $a, $b ) {
 }
 usort($feedArray, "sortFunction");
 
-//echo "<div class='feed'><strong>" . $feedArray[0]["title"] . "</strong><br>" . $feedArray[0]["description"] . "<br><span>" . time_elapsed_string($feedArray[0]["date"]) . "</span></div>";
+echo "<div class='feed'><strong>" . $feedArray[0]["title"] . "</strong><br>" . $feedArray[0]["description"] . "<br><span>" . time_elapsed_string($feedArray[0]["date"]) . "</span></div>";
 ?>
-<img src="img/IMG_3208.JPG" width="50%">
