3ca13767047afbbfb1d36bb1546b8c52dee769a7
Stephen Dance <stephen.dance@lightmaker.com>
2017-01-19 17:23:36 +0000

    New weather API and Icons

--- public/js/main.js
+++ public/js/main.js
@@ -7,7 +7,33 @@ var updateNews = function () {
     });
 };
 
+var updateWeather = function () {
+
+    var api_key = "3b94fb713b3d9d8cc30ebad237fa2892";
+    var units = "metric";
+    var location = "Tonbridge,uk";
+    $.ajax({
+
+        url: "http://api.openweathermap.org/data/2.5/weather?q="+location+"&units="+units+"&appid="+api_key,
+        success: function (data) {
+            nightorday = "d";
+            if (data.dt > data.sys.sunset) {
+                nightorday = "n";
+            }
+
+            html = '<div id="weather-icon" width="128" height="128"><i class="owf owf-5x owf-'+data.weather[0].id+'-'+nightorday+'"></i></div><h2> '+data.main.temp+'&deg;</h2>';
+            html += '<div id="region">'+data.name+'</div>';
+            html += '<div><i class="fa fa-angle-up"></i>  High '+data.main.temp_max + ' <i class="fa fa-angle-down"></i>  Low ' + data.main.temp_min + '</div>'
+            $("#weather").html(html);
+
+            //$("#forecast").html(html);
+        }
+    });
+};
+
 $(document).ready(function() {
     updateNews();
+    updateWeather();
     setInterval(updateNews, 60000);
+    setInterval(updateWeather, 60000);
 });
--- resources/views/mirror.blade.php
+++ resources/views/mirror.blade.php
@@ -8,15 +8,16 @@
         <meta name="description" content="Mirror mirror on the wall...">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <link rel="stylesheet" href="css/normalize.css">
-        <link rel="stylesheet" href="css/main.css">
-        <link rel="stylesheet" href="css/weather.css">
+        {{--<link rel="stylesheet" href="css/normalize.css">--}}
+        {{--<link rel="stylesheet" href="css/main.css">--}}
+        {{--<link rel="stylesheet" href="css/weather.css">--}}
+
+        <link rel="stylesheet" href="/js/vendor/owfont-master/css/owfont-regular.css">
+
         <!-- Fonts -->
         <link href='https://fonts.googleapis.com/css?family=Neucha' rel='stylesheet' type='text/css'>
         <link href='https://fonts.googleapis.com/css?family=Open+Sans:300' rel='stylesheet' type='text/css'>
         <script src="/js/vendor/jquery-1.11.3.min.js"></script>
-        <script src="/js/vendor/jquery.simpleWeather.min.js"></script>
-        <script src="/js/skycons.js"></script>
         <script src="/js/vendor/moment.js"></script>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
     </head>
@@ -31,9 +32,8 @@
         </section>
         <div id="forecast">Loading the weather forecast...</div>
         <div id="greeting">Loading BBC football news...</div>
-        <script type="text/javascript" src="/js/main.js"></script>
+        <script type="text/javascript" src="/js/main.js?v=1"></script>
         <script type="text/javascript" src="/js/clock.js"></script>
-        <script type="text/javascript" src="/js/weather.js"></script>
 
     </body>
 </html>
