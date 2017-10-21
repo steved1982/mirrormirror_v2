e729b118d37bdf385e4aea06a0714518cce9ee78
Stephen Dance <steve@genielocker.com>
2017-10-21 11:12:11 +0100

    styling

--- public/js/main.js
+++ public/js/main.js
@@ -27,17 +27,19 @@ var updateWeather = function () {
             html += '<div class="weather-text"><i class="fa fa-angle-up"></i>  High '+data.main.temp_max + ' <i class="fa fa-angle-down"></i>  Low ' + data.main.temp_min + '</div>'
             $("#weather").html(html);
         }
-        ,
+    });
+
+    $.ajax({
         url: "http://api.openweathermap.org/data/2.5/forecast?q="+location+"&units=metric&appid="+api_key,
         success: function (data) {
-            console.log(data);
+            //console.log(data);
 
             forecast = '<div class="forecast-feed">';
-            forecast += '<div style="width: 20%; text-align: center; float: left; margin-right:5px;">'+data.list[7].weather[0].main+'<br /><img src="http://openweathermap.org/img/w/' + data.list[7].weather[0].icon + '.png" /><br>'+data.list[7].main.temp+'&#8451;</div>';
-            forecast += '<div style="width: 20%; text-align: center; float: left; margin-right:5px;">'+data.list[15].weather[0].main+'<br /><img src="http://openweathermap.org/img/w/' + data.list[15].weather[0].icon + '.png" /><br>'+data.list[15].main.temp+'&#8451;</div>';
-            forecast += '<div style="width: 20%; text-align: center; float: left; margin-right:5px;">'+data.list[23].weather[0].main+'<br /><img src="http://openweathermap.org/img/w/' + data.list[23].weather[0].icon + '.png" /><br>'+data.list[23].main.temp+'&#8451;</div>';
-            forecast += '<div style="width: 20%; text-align: center; float: left; margin-right:5px;">'+data.list[31].weather[0].main+'<br /><img src="http://openweathermap.org/img/w/' + data.list[31].weather[0].icon + '.png" /><br>'+data.list[31].main.temp+'&#8451;</div>';
-            forecast += '<div style="width: 20%; text-align: center; float: left;">'+data.list[39].weather[0].main+'<br /><img src="http://openweathermap.org/img/w/' + data.list[39].weather[0].icon + '.png" /><br>'+data.list[39].main.temp+'&#8451;</div>';
+            forecast += '<div style="width: 20%; text-align: center; float: left;">'+data.list[7].weather[0].main+'<br /><img width="100px" src="http://openweathermap.org/img/w/' + data.list[7].weather[0].icon + '.png" /><br>'+data.list[7].main.temp+'<span>&#8451;</span></div>';
+            forecast += '<div style="width: 20%; text-align: center; float: left;">'+data.list[15].weather[0].main+'<br /><img width="100px" src="http://openweathermap.org/img/w/' + data.list[15].weather[0].icon + '.png" /><br>'+data.list[15].main.temp+'<span>&#8451;</span></div>';
+            forecast += '<div style="width: 20%; text-align: center; float: left;">'+data.list[23].weather[0].main+'<br /><img width="100px" src="http://openweathermap.org/img/w/' + data.list[23].weather[0].icon + '.png" /><br>'+data.list[23].main.temp+'<span>&#8451;</span></div>';
+            forecast += '<div style="width: 20%; text-align: center; float: left;">'+data.list[31].weather[0].main+'<br /><img width="100px" src="http://openweathermap.org/img/w/' + data.list[31].weather[0].icon + '.png" /><br>'+data.list[31].main.temp+'<span>&#8451;</span></div>';
+            forecast += '<div style="width: 20%; text-align: center; float: left;">'+data.list[39].weather[0].main+'<br /><img width="100px" src="http://openweathermap.org/img/w/' + data.list[39].weather[0].icon + '.png" /><br>'+data.list[39].main.temp+'<span>&#8451;</span></div>';
             forecast += '</div>';
 
             $("#forecast").html(forecast);
